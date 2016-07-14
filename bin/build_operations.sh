1729ce384dca3da81b1966d6192c97ae36ac9cf4
opsb <oliver@opsb.co.uk>
2016-07-14 10:50:59 +0100

    Use package.json to determine node version

--- bin/build_operations.sh
+++ bin/build_operations.sh
@@ -1,5 +1,10 @@
 install_node() {
-  local node_version=$1
+  # Look in package.json's engines.node field for a semver range
+  local semver_range=$($bp_dir/vendor/jq -r .engines.node $build_dir/package.json)
+
+  # Resolve node version using semver.io
+  local node_version=$(curl --silent --get --data-urlencode "range=${semver_range}" https://semver.io/node/resolve)
+
   # Download node from Heroku's S3 mirror of nodejs.org/dist
   head "Downloading and installing node"
   node_url="http://s3pository.heroku.com/node/v${node_version}/node-v${node_version}-linux-x64.tar.gz"
